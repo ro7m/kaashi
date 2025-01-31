9907b80aa1558c20848520d84eade232afbc6fb9
ro7m <robin.mundra@gmail.com>
2025-01-31 18:59:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wedding-invitation/', // Replace with your repository name
+  base: '/kaashi/',
 })
