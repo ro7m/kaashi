5448b215c2e8a8d465eddc94ce959626ffaa0a04
ro7m <robin.mundra@gmail.com>
2025-01-31 19:36:49 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/kaashi/',
-  build: {
-    rollupOptions: {
-      external: ['react/jsx-runtime', 'lucide-react']
+  resolve: {
+    alias: {
+      'react/jsx-runtime': 'react/jsx-runtime.js'
     }
   }
 })
